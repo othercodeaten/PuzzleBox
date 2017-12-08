aac5ce32f19629d27693e488162f9aab82eefa0d
projectpuzzlebucks <33557609+projectpuzzlebucks@users.noreply.github.com>
2017-12-08 13:26:10 -0500

    Update puzzlebox.ino

--- puzzlebox.ino
+++ puzzlebox.ino
@@ -3,39 +3,41 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <bluefruit.h>
-/*********** PINOUTS FOR SENSORS ***********/
+/*********** PINOUTS FOR SENSORS *************/
 int sensorPin = 2; /* this is for the thermal sensor */
-/*************** PHOTO CELL ****************/
+/*************** PHOTO CELL ******************/
 int photocellPin = 3;    // the cell and 10K pulldown are connected to a0
 int photocellReading;    // the analog reading from the sensor divider
 int LEDpin = 30;         // connect Red LED to pin 11 (PWM pin)
 int LEDbrightness;
-/**************** BALL TILT *****************/
-int inPin = 5;         // the number of the input pin
-int outPin = 30;       // the number of the output pin
-int LEDstate = HIGH;   // the current state of the output pin
-int reading;           // the current reading from the input pin
-int previous = LOW;    // the previous reading from the input pin
-/**************** BLUETOOTH ******************/
-// BLE Service
-BLEDis  bledis;
-BLEUart bleuart;
-BLEBas  blebas;
+int ledPin = 11;
+int buttonBpin = 7; // button status
+byte leds = 0;
+int light;
+int light2;
+bool test = HIGH;
 // Software Timer for blinking RED LED
 SoftwareTimer blinkTimer;
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long time = 0;         // the last time the output pin was toggled
 long debounce = 50;    // the debounce time, increase if the output flickers
-/***************** CONNECTOR BOOL **************/
-int CONNECT = 0;
+/**************** Temperature stuff ***********/
+double temperatureF;
+double temperatureC;
 /******************** PARAMS *******************/
+int c;
 int temp1;
 int temp2;
 int photo1;
 int photo2;
-bool rightsideup = HIGH;
-/************************************************/
+bool tilted;
+int wincount = 0;
+/*********************TIME STUFF**************************/
+unsigned long previousMillis = 0;
+unsigned long cleardisplayinterval = 5000;
+unsigned long currentMillis = 0;
+
 #define MANUFACTURER_ID 0x0008 /*for bluetooth connectivity*/
 
 #define OLED_RESET 29
@@ -53,185 +55,82 @@ void setup()   {
 
   /********************************* FROM BLUETOOTH LOOP ***********************/
   Serial.begin(115200);
-  Serial.println("Bluefruit52 BLEUART Example");
+  Serial.println("Welcome to Mario's Puzzle Box");
   Serial.println("---------------------------\n");
 
   // Initialize blinkTimer for 1000 ms and start it
-  blinkTimer.begin(1000, blink_timer_callback);
-  blinkTimer.start();
 
   // Setup the BLE LED to be enabled on CONNECT
   // Note: This is actually the default behaviour, but provided
   // here in case you want to control this LED manually via PIN 19
-  Bluefruit.autoConnLed(true);
-
-  Bluefruit.begin();
-  // Set max power. Accepted values are: -40, -30, -20, -16, -12, -8, -4, 0, 4
-  Bluefruit.setTxPower(4);
-  Bluefruit.setName("Marios Puzzle Machine");
-  Bluefruit.setConnectCallback(connect_callback);
-  Bluefruit.setDisconnectCallback(disconnect_callback);
-
-  // Configure and Start Device Information Service
-  bledis.setManufacturer("Adafruit Industries");
-  bledis.setModel("Bluefruit Feather52");
-  bledis.begin();
-
-  // Configure and Start BLE Uart Service
-  bleuart.begin();
-
-  // Start BLE Battery Service
-  blebas.begin();
-  blebas.write(100);
-
-  Serial.println("stugottz");
-
-  // Set up and start advertising
-  startAdv();
 
   Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
   Serial.println("Once connected, enter character(s) that you wish to send");
-
-  //loadparam();
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
   // init done
 
-  /************** BALL TILT STUFF ************/
-  pinMode(inPin, INPUT);
-  digitalWrite(inPin, HIGH);   // turn on the built in pull-up resistor
-  pinMode(outPin, OUTPUT);
-  /*******************************************/
-
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
   display.display();
 
   // Clear the buffer.
   display.clearDisplay();
+  randomSeed(analogRead(28));
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonBpin, INPUT_PULLUP);
+
+  initializer();
+  initprinter();
 }
 
 void loop() {
-  blue();
-  tiltevent();
-  displaypr();
-  displaytemp();
-}
 
-void blue () {
+  currentMillis = millis();
 
-  // Forward data from HW Serial to BLEUART
-  while (Serial.available())
-  {
-    // Delay to wait for enough input, since we have a limited transmission buffer
-    delay(2);
+  displayall();
 
-    uint8_t buf[64];
-    int count = Serial.readBytes(buf, sizeof(buf));
-    bleuart.write( buf, count );
+  while (wincount != 1) {
+    gettemp();
+    getpr();
+    displayall();
+
+    if (digitalRead(buttonBpin) == HIGH)
+    {
+      if (light == 0) {
+        light2 = 1;
+      }
+      digitalWrite(ledPin, LOW);
+    }
+    if (digitalRead(buttonBpin) == LOW)
+    {
+      if (light == 1) {
+        light2 = 1;
+      }
+      digitalWrite(ledPin, HIGH);
+    }
+
+    if (((temperatureF >= temp1) && (temperatureF <= temp2)) &&
+        ((photocellReading >= photo1) && (photocellReading <= photo2))
+        && (light2 == 1)) {
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 0);
+      display.println("YOU WON THE GAME!");
+      display.display();
+      //display.clearDisplay();
+      wincount++;
+      delay(5000);
+      initializer();
+      initprinter();
+    }
+    wincount--;
   }
-
-  // Forward from BLEUART to HW Serial
-  while ( bleuart.available() )
-  {
-    uint8_t ch;
-    ch = (uint8_t) bleuart.read();
-    Serial.write(ch);
-  }
-
-  // Request CPU to enter low-power mode until an event/interrupt occurs
-  waitForEvent();
-
+  //delay(3000);
 }
 
-void startAdv(void)
-{
-  // Advertising packet
-  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addTxPower();
-
-  // Include bleuart 128-bit uuid
-  Bluefruit.Advertising.addService(bleuart);
-
-  // Secondary Scan Response packet (optional)
-  // Since there is no room for 'Name' in Advertising packet
-  Bluefruit.ScanResponse.addName();
-
-  /* Start Advertising
-     - Enable auto advertising if disconnected
-     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-     - Timeout for fast mode is 30 seconds
-     - Start(timeout) with timeout = 0 will advertise forever (until connected)
-
-     For recommended advertising interval
-     https://developer.apple.com/library/content/qa/qa1931/_index.html
-  */
-  Bluefruit.Advertising.restartOnDisconnect(true);
-  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
-}
-
-
-void connect_callback(uint16_t conn_handle)
-{
-  char central_name[32] = { 0 };
-  Bluefruit.Gap.getPeerName(conn_handle, central_name, sizeof(central_name));
-
-  Serial.print("Connected to ");
-  Serial.println(central_name);
-
-  CONNECT = 1;
-}
-
-void disconnect_callback(uint16_t conn_handle, uint8_t reason)
-{
-  (void) conn_handle;
-  (void) reason;
-
-  Serial.println();
-  Serial.println("Disconnected");
-
-  CONNECT = 0;
-}
-
-/**
-   Software Timer callback is invoked via a built-in FreeRTOS thread with
-   minimal stack size. Therefore it should be as simple as possible. If
-   a periodically heavy task is needed, please use Scheduler.startLoop() to
-   create a dedicated task for it.
-
-   More information http://www.freertos.org/RTOS-software-timer.html
-*/
-void blink_timer_callback(TimerHandle_t xTimerID)
-{
-  (void) xTimerID;
-  digitalToggle(LED_RED);
-}
-
-/**
-   RTOS Idle callback is automatically invoked by FreeRTOS
-   when there are no active threads. E.g when loop() calls delay() and
-   there is no bluetooth or hw event. This is the ideal place to handle
-   background data.
-
-   NOTE: FreeRTOS is configured as tickless idle mode. After this callback
-   is executed, if there is time, freeRTOS kernel will go into low power mode.
-   Therefore waitForEvent() should not be called in this callback.
-   http://www.freertos.org/low-power-tickless-rtos.html
-
-   WARNING: This function MUST NOT call any blocking FreeRTOS API
-   such as delay(), xSemaphoreTake() etc ... for more information
-   http://www.freertos.org/a00016.html
-*/
-void rtos_idle_callback(void)
-{
-  // Don't call any other FreeRTOS blocking API()
-  // Perform background task(s) here
-}
-void displaytemp() {
+void gettemp() {
 
   int reading = analogRead(sensorPin);
 
@@ -240,118 +139,96 @@ void displaytemp() {
   voltage /= 1024.0;
 
   // now print out the temperature
-  float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+  temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
   //to degrees ((voltage - 500mV) times 100)
 
   // now convert to Fahrenheit
-  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
   /******* PRINT DEGREE C ****************/
   /* NOTE: there was a weird thing with how this prints on the LCD*/
   // show that its in degree C
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(temperatureC); display.println("Degrees C");
-  display.display();
 
+  display.clearDisplay();
+}
+
+void getpr() {
+
+  photocellReading = analogRead(photocellPin);
+
+
+
+}
+
+// intialize vars...
+void initializer() {
+
+  int val = random(0, 100);
+
+  if (val >= 50) { //darkp
+    temp1 = random(60, 70);
+    temp2 = random(80, 90);
+    photo1 = random(10, 40);
+    photo2 = random(50, 70);
+    light = 0;
+
+  } else { //light
+    temp1 = random(60, 70);
+    temp2 = random(80, 90);
+    photo1 = random(500, 600);
+    photo2 = random(750, 800);
+    light = 1;
+  }
+}
+// display vars
+void initprinter() {
+  Serial.println(temp1);
+  Serial.println(temp2);
+  Serial.println(photo1);
+  Serial.println(photo2);
+}
+void displayall() {
+
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(70, 0);
+  display.setCursor(0, 10);
+  display.println("Temp:");
+
+  /********* PRINT DEGREE F ****************/
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(60, 10);
   display.println(temperatureF);
   display.display();
-  /*****************************************/
-  /********* PRINT DEGREE F *****************/
-  // show that its deg F
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(70, 8);
-  display.println("Degrees F");
-  display.display();
-
-  // show the voltage
+  /********* PRINT DEGREE F ****************/
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 20);
-  display.println("Voltage is: ");
-  display.setCursor(70, 20);
-  display.println(voltage);
+  display.println("Lumens:");
   display.display();
-  /********************************************/
 
-  // delay(2000);  //waiting 2 seconds
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(60, 20);
+  display.println(photocellReading);
+  display.display();
 
-  // this condition true if....
-  if (temperatureF >= 95 && temperatureC >= 32.2222) {
+  if (light == 0 ) {
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.setCursor(0, 0);
-    display.println("TOO HOT!!!");
+    display.println("TURN ON THE LIGHT");
     display.display();
-    //delay(2000);
   }
 
+  if (light == 1) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("TURN OFF THE LIGHT");
+    display.display();
+  }
   display.clearDisplay();
-} 
-void loadparam() {
-
-  /*int temp1;
-    int temp2;
-    int photo1;
-    int photo2;
-    bool rightsideup = HIGH; */
-
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Enter Temp Range: ");
-  display.display();
-
-  delay(2000);
-
-
-}
-void displaypr() {
-
-  photocellReading = analogRead(photocellPin);
-
-  // Serial.print("Analog reading = ");
-  //Serial.println(photocellReading);     // the raw analog reading
-
-  // LED gets brighter the darker it is at the sensor
-  // that means we have to -invert- the reading from 0-1023 back to 1023-0
-  photocellReading = 1023 - photocellReading;
-  //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-  //LEDbrightness = map(photocellReading, 0, 1023, 0, 255);
-  //analogWrite(LEDpin, LEDbrightness);
-
-  //delay(100);
-}
-void tiltevent() {
-
-  int switchstate;
-
-  reading = digitalRead(inPin);
-
-  // If the switch changed, due to bounce or pressing...
-  if (reading != previous) {
-    // reset the debouncing timer
-    time = millis();
-  }
-
-  if ((millis() - time) > debounce) {
-    // whatever the switch is at, its been there for a long time
-    // so lets settle on it!
-    switchstate = reading;
-
-    // Now invert the output on the pin13 LED
-    if (switchstate == HIGH)
-      LEDstate = LOW;
-    else
-      LEDstate = HIGH;
-  }
-  digitalWrite(outPin, LEDstate);
-
-  // Save the last reading so we keep a running tally
-  previous = reading;
 }
